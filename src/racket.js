170aa1be5d836f4315f6bd37228912baba45bd64
leslieyip02 <leslieyip02@gmail.com>
2022-07-10 00:21:13 +0800

    Troubleshooting github page deployment

--- src/racket.js
+++ src/racket.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import racketData from "../models/racket.glb?url";
 
 export class Racket {
     constructor(scene, loader) {
@@ -9,8 +10,9 @@ export class Racket {
     }
 
     async render() {
-        const racketData = await this.loader.loadAsync("../models/racket.glb"); 
-        const racketModel = await racketData.scene.children[0];
+        const racketModel = await this.loader.loadAsync(racketData)
+            .then(data => data.scene.children[0]);
+        
         racketModel.position.set(10, 10, 10);
 
         this.scene.add(racketModel);
