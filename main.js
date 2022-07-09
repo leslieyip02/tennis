e3064c8a970d81c533ff12433901b29301ae5aea
leslieyip02 <leslieyip02@gmail.com>
2022-07-09 18:42:18 +0800

    Updated for deployment to github pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
-import { OrbitControls } from "orbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Court } from "./src/court.js";
 import { Racket } from "./src/racket.js";
--- src/racket.js
+++ src/racket.js
@@ -1,5 +1,3 @@
-import * as THREE from "three";
-
 export class Racket {
     constructor(scene, loader) {
         this.scene = scene;
